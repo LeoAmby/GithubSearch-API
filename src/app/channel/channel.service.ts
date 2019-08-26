f0208724d5df461430cefadf8d597e7328777543
Leo <leoamby@gmail.com>
2019-08-26 12:15:17 +0300

    import the user-id and repositories classes to the channel service

---
+++ src/app/channel/channel.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { UserId } from '../user-id';
+import { Repositories } from '../repositories';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChannelService {
+
+  constructor() { }
+}
--- src/app/user-id.ts
+++ src/app/user-id.ts
@@ -1,2 +1,5 @@
 export class UserId {
+    constructor(public name: string, public email: string, public company:string, public bio:string){
+
+    }
 }
